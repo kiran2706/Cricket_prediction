57c72b95305ead101da006f82790b8f4b0e8fd22
Kiran R <71453237+kiran2706@users.noreply.github.com>
2021-04-13 14:09:56 +0530

    Update Cricket_stream.py

--- Cricket_stream.py
+++ Cricket_stream.py
@@ -2,7 +2,7 @@ from pycaret.classification import load_model, predict_model
 import streamlit as st
 import pandas as pd
 import datetime
-model = load_model('Scrap_cricket_model ')
+model = load_model('Scrap_cricket_model')
 
 st.markdown('<style>body{background-color: Blue;}</style>',unsafe_allow_html=True)
 
