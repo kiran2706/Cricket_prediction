48905da526732befd4e7ad285270a6a186dfd66f
Kiran R <71453237+kiran2706@users.noreply.github.com>
2021-04-21 10:48:51 +0530

    Update Cricket_stream.py

--- Cricket_stream.py
+++ Cricket_stream.py
@@ -8,7 +8,7 @@ st.markdown(
     """
 <style>
 .sidebar .sidebar-content {
-    background-image: linear-gradient(lightblue);
+    background-image: linear-gradient(lightblue,lightblue);
     color: lightblue;
 }
 </style>
