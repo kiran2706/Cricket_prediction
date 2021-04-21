e300e7c61b08f68ca7be27c0cb9dac02aa933281
Kiran R <71453237+kiran2706@users.noreply.github.com>
2021-04-21 10:41:55 +0530

    Update Cricket_stream.py

--- Cricket_stream.py
+++ Cricket_stream.py
@@ -25,7 +25,8 @@ def run():
     ("Online", "Batch"))
     st.sidebar.info('This app was created to predict the chances of the Indian team to win or loose with an accuracy of 80%')
     st.sidebar.success('''Data was scraped from the following website - 
-    http://www.howstat.com/cricket/Statistics/Matches/MatchListCountry_ODI.asp?A=IND''')
+    http://www.howstat.com/cricket/Statistics/Matches
+    /MatchListCountry_ODI.asp?A=IND''')
     st.sidebar.image(image_sidebar,use_column_width=True)
     st.title("Predicting ODI match Result")
     if add_selectbox == 'Online':
